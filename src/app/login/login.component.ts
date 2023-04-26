7f5b5cda24405972fc49b65fdbbd77061dbb2099
OssFit <oalatrista@gmail.com>
2023-04-25 19:00:32 -0500

    upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string | undefined;
+  password:string | undefined;
+
+  onSubmit(){
+    console.log('login submit');
+  }
+
+}
