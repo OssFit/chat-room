43cc2423e4b92c4b38540b957c7329cea4f64628
PJ <pablo@jdiaz.com>
2023-04-26 09:13:17 -0600

    Chat Page partition inlcuding search bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent,
+    },
+  {path:'chatroom', component:ChatroomComponent,
+    },
 ];
 
 @NgModule({
